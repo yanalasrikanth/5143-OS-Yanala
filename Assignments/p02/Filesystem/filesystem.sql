f6bdcc42405d1f2a1a7c7d040bff639bd11b8bff
yanalasrikanth <85611993+yanalasrikanth@users.noreply.github.com>
2023-11-06 23:49:20 -0600

    Update filesystem.sql

--- Assignments/p02/Filesystem/filesystem.sql
+++ Assignments/p02/Filesystem/filesystem.sql
@@ -1,9 +1,13 @@
 INSERT INTO FileSystem (pid, filename, file_type, file_size, owner,"group", permissions, modification_time, content, hidden)
-   VALUES(0,'file.txt','FileSystem','32','root','root','rwxr-xr-x','2023-10-26 14:00:00 UTC',NULL,false);
+   VALUES(0,'/','directory','NULL','root','root','rwxr-xr-x','2023-09-13 14:00:00 UTC',NULL,false);
 INSERT INTO FileSystem (pid, filename, file_type, file_size, owner,"group", permissions, modification_time, content, hidden)
-   VALUES(1,'file1.txt','FileSystem','1024','root','root','rwxr-xr-x','2023-10-26 14:05:10 UTC',NULL,false);
+   VALUES(1,'home','directory','NULL','root','root','rwxr-xr-x','2023-11-08 14:00:00 UTC',NULL,false);
 INSERT INTO FileSystem (pid, filename, file_type, file_size, owner,"group", permissions, modification_time, content, hidden)
-   VALUES(2,'file2.txt','FileSystem','1157','root','root','rwxr-xr-x','2023-10-26 14:20:04 UTC',NULL,false);
+   VALUES(1,'home/file.txt','file','32','root','root','rwxr-xr-x','2023-10-26 14:00:00 UTC',NULL,false);
 INSERT INTO FileSystem (pid, filename, file_type, file_size, owner,"group", permissions, modification_time, content, hidden)
-   VALUES(3,'file3.txt','FileSystem','2304','root','root','rwxr-xr-x','2023-10-26 14:21:00 UTC',NULL,false);
+   VALUES(1,'home/file1.txt','file','1024','root','root','rwxr-xr-x','2023-10-26 14:05:10 UTC',NULL,false);
+INSERT INTO FileSystem (pid, filename, file_type, file_size, owner,"group", permissions, modification_time, content, hidden)
+   VALUES(1,'home/file2.txt','file','1157','root','root','rwxr-xr-x','2023-10-26 14:20:04 UTC',NULL,false);
+INSERT INTO FileSystem (pid, filename, file_type, file_size, owner,"group", permissions, modification_time, content, hidden)
+   VALUES(1,'home/file3.txt','file','2304','root','root','rwxr-xr-x','2023-10-26 14:21:00 UTC',NULL,false);
 SELECT * from FileSystem;
