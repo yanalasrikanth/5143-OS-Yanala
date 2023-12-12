5c4fa3b99e330c224c9aecbf8fa1d1cc77391df2
yanalasrikanth <85611993+yanalasrikanth@users.noreply.github.com>
2023-12-11 22:11:28 -0600

    Update io_dev.py

--- Assignments/P03/io_dev.py
+++ Assignments/P03/io_dev.py
@@ -1,1 +1,35 @@
+class IO:
+    def __init__(self) -> None:
+        self.busy = False
+        self.servingPCB = None
 
+    def decrementCurrentProcess(self):
+        """
+        Decrements the current IO burst time of the process being served.
+        """
+        if self.servingPCB is not None:
+            self.servingPCB.decrementIoBurst()
+
+    def loadProcess(self, pcb):
+        """
+        Loads a process onto the IO device.
+        """
+        self.servingPCB = pcb
+        self.busy = True  # Mark the IO device as busy
+
+    def unloadProcess(self):
+        """
+        Unloads the process from the IO device and returns it.
+        """
+        if self.servingPCB is not None and self.servingPCB.isIoBurstComplete():
+            finished_process = self.servingPCB
+            self.servingPCB = None
+            self.busy = False  # Mark the IO device as available
+            return finished_process
+        return None
+
+    def isBusy(self):
+        """
+        Returns True if the IO device is currently busy.
+        """
+        return self.busy
