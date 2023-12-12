7c2023720cbb3aca095272ea08a59f63321e7a83
yanalasrikanth <85611993+yanalasrikanth@users.noreply.github.com>
2023-12-11 22:11:56 -0600

    Update cpu.py

--- Assignments/P03/cpu.py
+++ Assignments/P03/cpu.py
@@ -1,1 +1,27 @@
+class CPU:
+    def __init__(self):
+        self.busy = False
+        self.runningPCB = None
+        self.executionTime = 0
 
+    def incrementExecutionTime(self):
+        self.executionTime += 1
+
+    def decrementCurrentProcess(self):
+        self.runningPCB.decrementCpuBurst()
+
+    def loadProcess(self, pcb):
+        self.runningPCB = pcb
+
+    def KickOff(self):
+        if self.runningPCB.getCurrentBurstTime() == 0:
+            self.busy = False
+            item = self.runningPCB
+            self.runningPCB = None
+            return item
+    def unloadProcess(self):
+      
+        finishedProcess = self.runningPCB
+        self.runningPCB = None
+        self.busy = False  # Mark the CPU as not busy
+        return finishedProcess
